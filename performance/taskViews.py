227361adf0d7164cd65fa1ae0231c1fc99d03e3d
leeyoshinari <lee_jc@outlook.com>
2022-09-14 22:07:53 +0800

    fix

--- performance/taskViews.py
+++ performance/taskViews.py
@@ -398,22 +398,22 @@ def get_running_server(request):
             username = request.user.username
             task_id = request.GET.get('id')
             hosts = TestTaskLogs.objects.values('value', 'action').filter(task_id=task_id, action=1)
-            # host_info = []
-            # for h in hosts:
-            #     host_dict = get_value_by_host('jmeterServer_' + h.value)
-            #     host_dict.update(get_value_by_host('Server_' + h.value))
-            #     host_dict.update({'action': h.action})
-            #     host_info.append(host_dict)
-            host_info = [
-                {'host': '127.0.0.2', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
-                 'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
-                 'status': 0, 'action':1, 'tps': 20}, {'host': '127.0.0.3', 'status': 1, 'action': 2, 'tps': 0},
-                {'host': '127.0.0.4', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
-                 'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
-                 'status': 2, 'action':1},
-                {'host': '127.0.0.5', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
-                 'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
-                 'status': 1, 'action':1}]
+            host_info = []
+            for h in hosts:
+                host_dict = get_value_by_host('jmeterServer_' + h.value)
+                host_dict.update(get_value_by_host('Server_' + h.value))
+                host_dict.update({'action': h.action})
+                host_info.append(host_dict)
+            # host_info = [
+            #     {'host': '127.0.0.2', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
+            #      'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
+            #      'status': 0, 'action':1, 'tps': 20}, {'host': '127.0.0.3', 'status': 1, 'action': 2, 'tps': 0},
+            #     {'host': '127.0.0.4', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
+            #      'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
+            #      'status': 2, 'action':1},
+            #     {'host': '127.0.0.5', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
+            #      'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
+            #      'status': 1, 'action':1}]
             logger.info(f'Query running servers success, operator: {username}')
             return result(msg='Get running servers success ~', data=host_info)
         except:
@@ -427,22 +427,22 @@ def get_idle_server(request):
             username = request.user.username
             server_room_id = request.GET.get('id')
             servers = Servers.objects.values('host').filter(Q(room_id=server_room_id), Q(room__type=2))
-            # host_info = []
-            # for server in servers:
-            #     host_dict = get_value_by_host('jmeterServer_' + server.host)
-            #     if host_dict['status'] == 0:
-            #         host_dict.update(get_value_by_host('Server_' + server.host))
-            #         host_info.append(host_dict)
-            host_info = [
-                {'host': '127.0.0.2', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
-                 'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
-                 'status': 0}, {'host': '127.0.0.3', 'status': 1},
-                {'host': '127.0.0.4', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
-                 'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
-                 'status': 2},
-                {'host': '127.0.0.5', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
-                 'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
-                 'status': 1}]
+            host_info = []
+            for server in servers:
+                host_dict = get_value_by_host('jmeterServer_' + server.host)
+                if host_dict['status'] == 0:
+                    host_dict.update(get_value_by_host('Server_' + server.host))
+                    host_info.append(host_dict)
+            # host_info = [
+            #     {'host': '127.0.0.2', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
+            #      'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
+            #      'status': 0}, {'host': '127.0.0.3', 'status': 1},
+            #     {'host': '127.0.0.4', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
+            #      'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
+            #      'status': 2},
+            #     {'host': '127.0.0.5', 'port': 89, 'system': 'centos', 'cpu': 4, 'mem': 3.5, 'disk': '3G', 'nic': 'eno1',
+            #      'network_speed': 1000, 'disk_size': 3, 'mem_usage': 32, 'cpu_usage': 26, 'disk_usage': 12,
+            #      'status': 1}]
             logger.info(f'Query idle servers success, operator: {username}')
             return result(msg='Get idle servers success ~', data=host_info)
         except:
@@ -468,7 +468,8 @@ def query_data(request):
 
 
 def get_data_from_influx(task_id, host=None, start_time=None, end_time=None):
-    query_data = {'time': ['2022-08-20 23:50:00','2022-08-20 23:58:10','2022-08-21 00:00:01'], 'samples': [20, 46, 97], 'tps': [5, 7, 20], 'avg_rt': [345, 378, 567], 'min_rt': [123, 102, 120], 'max_rt': [678, 567, 969], 'err': [1, 3, 2], 'active': [150, 200, 200]}
+    # query_data = {'time': ['2022-08-20 23:50:00','2022-08-20 23:58:10','2022-08-21 00:00:01'], 'samples': [20, 46, 97], 'tps': [5, 7, 20], 'avg_rt': [345, 378, 567], 'min_rt': [123, 102, 120], 'max_rt': [678, 567, 969], 'err': [1, 3, 2], 'active': [150, 200, 200]}
+    query_data = {'time': [], 'samples': [], 'tps': [], 'avg_rt': [], 'min_rt': [], 'max_rt': [], 'err': [], 'active': []}
     res = {'code': 0, 'data': None, 'message': 'Query InfluxDB Successful!'}
     try:
         conn = influxdb.InfluxDBClient(settings.INFLUX_HOST, settings.INFLUX_PORT, settings.INFLUX_USER_NAME,
